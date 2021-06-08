851ce92dcdadc008159e3112e40300e1aabfd8a9
Ayman Elamrani <aelamran@e1r6p8.1337.ma>
2021-06-08 19:51:18 +0100

    fixing some bugs, ready to launch

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from 'react-dom'
+import InputTag from "./InputTag"
+
+ReactDOM.render(
+    <div style={{
+        width: "200px",
+        height: "200px"
+    }}>
+        <InputTag />
+    </div>,
+    document.getElementById("root")
+)
